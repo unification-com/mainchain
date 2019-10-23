72675a5ebc2acd7d03d9520d8b932c0501316b2b
Codegnosis <paul@codegnosis.co.uk>
2019-10-23 11:09:20 +0100

    x.wrkchain - add internal keeper/query functionality for recording WRKChain block hashes

--- x/wrkchain/internal/keeper/querier.go
+++ x/wrkchain/internal/keeper/querier.go
@@ -2,6 +2,7 @@ package keeper
 
 import (
 	"github.com/cosmos/cosmos-sdk/codec"
+	"strconv"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	abci "github.com/tendermint/tendermint/abci/types"
@@ -9,7 +10,8 @@ import (
 
 // query endpoints supported by the wrkchain Querier
 const (
-	QueryWrkChain   = "get"
+	QueryWrkChain      = "get"
+	QueryWrkChainBlock = "get-block"
 )
 
 // NewQuerier is the module level router for state queries
@@ -18,6 +20,8 @@ func NewQuerier(keeper Keeper) sdk.Querier {
 		switch path[0] {
 		case QueryWrkChain:
 			return queryWrkChain(ctx, path[1:], req, keeper)
+		case QueryWrkChainBlock:
+			return queryWrkChainBlock(ctx, path[1:], req, keeper)
 		default:
 			return nil, sdk.ErrUnknownRequest("unknown wrkchain query endpoint")
 		}
@@ -26,9 +30,27 @@ func NewQuerier(keeper Keeper) sdk.Querier {
 
 // nolint: unparam
 func queryWrkChain(ctx sdk.Context, path []string, req abci.RequestQuery, keeper Keeper) ([]byte, sdk.Error) {
-	whois := keeper.GetWrkChain(ctx, path[0])
+	wrkchain := keeper.GetWrkChain(ctx, path[0])
 
-	res, err := codec.MarshalJSONIndent(keeper.cdc, whois)
+	res, err := codec.MarshalJSONIndent(keeper.cdc, wrkchain)
+	if err != nil {
+		panic("could not marshal result to JSON")
+	}
+
+	return res, nil
+}
+
+func queryWrkChainBlock(ctx sdk.Context, path []string, req abci.RequestQuery, keeper Keeper) ([]byte, sdk.Error) {
+
+	height, err := strconv.Atoi(path[1])
+
+	if err != nil {
+		height = 0
+	}
+
+	wrkchainBlock := keeper.GetWrkChainBlock(ctx, path[0], uint64(height))
+
+	res, err := codec.MarshalJSONIndent(keeper.cdc, wrkchainBlock)
 	if err != nil {
 		panic("could not marshal result to JSON")
 	}

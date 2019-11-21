efa92a1d42a7d7444416cdcc382156a28c418a62
Indika Piyasena <indika.piyasena@gmail.com>
2019-11-21 12:52:02 +0900

    add a basic keeper

---
+++ x/beacon/internal/keeper/querier.go
@@ -0,0 +1,35 @@
+package keeper
+
+import (
+	"github.com/cosmos/cosmos-sdk/codec"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	abci "github.com/tendermint/tendermint/abci/types"
+)
+
+// query endpoints supported by the beacon Querier
+const (
+	QueryParameters          = "params"
+)
+
+// NewQuerier is the module level router for state queries
+func NewQuerier(keeper Keeper) sdk.Querier {
+	return func(ctx sdk.Context, path []string, req abci.RequestQuery) (res []byte, err sdk.Error) {
+		switch path[0] {
+		case QueryParameters:
+			return queryParams(ctx, keeper)
+		default:
+			return nil, sdk.ErrUnknownRequest("unknown beacon query endpoint")
+		}
+	}
+}
+
+func queryParams(ctx sdk.Context, k Keeper) ([]byte, sdk.Error) {
+	params := k.GetParams(ctx)
+
+	res, err := codec.MarshalJSONIndent(k.cdc, params)
+	if err != nil {
+		return nil, sdk.ErrInternal(sdk.AppendMsgToErr("failed to marshal JSON", err.Error()))
+	}
+
+	return res, nil
+}

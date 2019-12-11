8f0749c7b93e4448015f7a693123a2d27fe80d3f
Codegnosis <paul@codegnosis.co.uk>
2019-12-11 13:15:05 +0000

    x.enterprise - supply and auth/account REST overrides to include locked UND data

--- x/enterprise/client/rest/rest.go
+++ x/enterprise/client/rest/rest.go
@@ -17,3 +17,14 @@ func RegisterRoutes(cliCtx context.CLIContext, r *mux.Router) {
 	registerQueryRoutes(cliCtx, r)
 	registerTxRoutes(cliCtx, r)
 }
+
+// RegisterAuthAccountOverride registers a REST route to override the default /auth/accounts/{address} path
+// and additionally return Enterprise Locked UND data
+func RegisterAuthAccountOverride(cliCtx context.CLIContext, r *mux.Router) {
+	registerEnterpriseAuthAccountOverride(cliCtx, r)
+}
+
+func RegisterTotalSupplyOverride(cliCtx context.CLIContext, r *mux.Router) {
+	registerEnterpriseTotalSupplyOverride(cliCtx, r)
+	registerEnterpriseSupplyByDenomOverride(cliCtx, r)
+}

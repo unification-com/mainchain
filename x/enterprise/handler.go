bbade7e41585107bd19355bbe5819ddab2fb6af3
Codegnosis <paul@codegnosis.co.uk>
2020-04-03 15:03:54 +0100

    x.enteprise - whitelist msg handler

--- x/enterprise/handler.go
+++ x/enterprise/handler.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+	"github.com/unification-com/mainchain/x/enterprise/internal/types"
 	"strconv"
 )
 
@@ -15,6 +16,8 @@ func NewHandler(keeper Keeper) sdk.Handler {
 			return handleMsgPurchaseUnd(ctx, keeper, msg)
 		case MsgProcessUndPurchaseOrder:
 			return handleMsgProcessPurchaseUnd(ctx, keeper, msg)
+		case types.MsgWhitelistAddress:
+			return handleMsgWhitelistAddress(ctx, keeper, msg)
 		default:
 			return nil, sdkerrors.Wrapf(sdkerrors.ErrUnknownRequest, "unrecognized %s message type: %T", ModuleName, msg)
 		}
@@ -25,6 +28,10 @@ func handleMsgPurchaseUnd(ctx sdk.Context, k Keeper, msg MsgPurchaseUnd) (*sdk.R
 
 	if msg.Amount.Denom != k.GetParamDenom(ctx) {
 		return nil, sdkerrors.Wrap(ErrInvalidDenomination, fmt.Sprintf("denomination must be %s", k.GetParamDenom(ctx)))
+	}
+
+	if !k.AddressIsWhitelisted(ctx, msg.Purchaser) {
+		return nil, sdkerrors.Wrap(ErrNotAuthorisedToRaisePO,  fmt.Sprintf("%s is not whitelisted", msg.Purchaser))
 	}
 
 	purchaseOrderID, err := k.RaiseNewPurchaseOrder(ctx, msg.Purchaser, msg.Amount)
@@ -80,4 +87,35 @@ func handleMsgProcessPurchaseUnd(ctx sdk.Context, k Keeper, msg MsgProcessUndPur
 		Events: ctx.EventManager().Events(),
 		Data:   retData,
 	}, nil
+}
+
+func handleMsgWhitelistAddress(ctx sdk.Context, k Keeper, msg types.MsgWhitelistAddress) (*sdk.Result, error) {
+	// check only authorised Enterprise account is adding/removing
+	if !k.IsAuthorisedToDecide(ctx, msg.Signer) {
+		return nil, sdkerrors.Wrap(sdkerrors.ErrUnauthorized, "unauthorised signer modifying whitelist")
+	}
+
+	if !ValidWhitelistAction(msg.Action) {
+		return nil, sdkerrors.Wrap(ErrInvalidDecision, "action should be add or remove")
+	}
+
+	err := k.ProcessWhitelistAction(ctx, msg.Address, msg.Action, msg.Signer)
+
+	if err != nil {
+		return nil, err
+	}
+
+	ctx.EventManager().EmitEvents(sdk.Events{
+		sdk.NewEvent(
+			EventTypeWhitelistAddress,
+			sdk.NewAttribute(AttributeWhitelistAddress, msg.Address.String()),
+			sdk.NewAttribute(AttributeKeySigner, msg.Signer.String()),
+			sdk.NewAttribute(AttributeKeyWhitelistAction, msg.Action.String()),
+		),
+	})
+
+	return &sdk.Result{
+		Events: ctx.EventManager().Events(),
+	}, nil
+
 }

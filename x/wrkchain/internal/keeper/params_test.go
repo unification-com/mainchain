10f2cc125da72ab1ce4653a7efc5347e2d74c9b6
Codegnosis <paul@codegnosis.co.uk>
2020-04-15 14:50:29 +0100

    x.wrkchain - wider test coverage

--- x/wrkchain/internal/keeper/params_test.go
+++ x/wrkchain/internal/keeper/params_test.go
@@ -1,27 +1,117 @@
 package keeper
 
 import (
-	"github.com/stretchr/testify/require"
 	"testing"
 
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/stretchr/testify/require"
 	"github.com/unification-com/mainchain/x/wrkchain/internal/types"
 )
 
-// Tests for Highest Purchase Order ID
+var testParams = types.NewParams(1000,100, "nund")
 
 func TestSetGetParams(t *testing.T) {
 	ctx, _, keeper := createTestInput(t, false, 100, 0)
-	newFeeReg := uint64(1000)
-	newFeeRec := uint64(100)
-	denom := "somecoin"
-	params := types.NewParams(newFeeReg, newFeeRec, denom)
 
-	keeper.SetParams(ctx, params)
+	keeper.SetParams(ctx, testParams)
 
 	paramsDb := keeper.GetParams(ctx)
 
-	require.True(t, ParamsEqual(params, paramsDb))
-	require.True(t, paramsDb.FeeRegister == newFeeReg)
-	require.True(t, paramsDb.FeeRecord == newFeeRec)
-	require.True(t, paramsDb.Denom == denom)
+	require.True(t, ParamsEqual(testParams, paramsDb))
+	require.True(t, paramsDb.FeeRegister == testParams.FeeRegister)
+	require.True(t, paramsDb.FeeRecord == testParams.FeeRecord)
+	require.True(t, paramsDb.Denom == testParams.Denom)
+}
+
+func TestGetParamDenom(t *testing.T) {
+	ctx, _, keeper := createTestInput(t, false, 100, 0)
+	keeper.SetParams(ctx, testParams)
+
+	ret := keeper.GetParamDenom(ctx)
+
+	require.Equal(t, ret, testParams.Denom)
+}
+
+func TestGetParamRegistrationFee(t *testing.T) {
+	ctx, _, keeper := createTestInput(t, false, 100, 0)
+	keeper.SetParams(ctx, testParams)
+
+	ret := keeper.GetParamRegistrationFee(ctx)
+
+	require.Equal(t, ret, testParams.FeeRegister)
+}
+
+func TestGetParamRecordFee(t *testing.T) {
+	ctx, _, keeper := createTestInput(t, false, 100, 0)
+	keeper.SetParams(ctx, testParams)
+
+	ret := keeper.GetParamRecordFee(ctx)
+
+	require.Equal(t, ret, testParams.FeeRecord)
+}
+
+func TestGetZeroFeeAsCoin(t *testing.T) {
+	ctx, _, keeper := createTestInput(t, false, 100, 0)
+	keeper.SetParams(ctx, testParams)
+
+	ret := keeper.GetZeroFeeAsCoin(ctx)
+
+	paramCoin := sdk.NewInt64Coin(testParams.Denom, 0)
+
+	require.True(t, ret.IsEqual(paramCoin))
+}
+
+func TestGetRegistrationFeeAsCoin(t *testing.T) {
+	ctx, _, keeper := createTestInput(t, false, 100, 0)
+	keeper.SetParams(ctx, testParams)
+
+	ret := keeper.GetRegistrationFeeAsCoin(ctx)
+
+	paramCoin := sdk.NewInt64Coin(testParams.Denom, int64(testParams.FeeRegister))
+
+	require.True(t, ret.IsEqual(paramCoin))
+}
+
+func TestGetRecordFeeAsCoin(t *testing.T) {
+	ctx, _, keeper := createTestInput(t, false, 100, 0)
+	keeper.SetParams(ctx, testParams)
+
+	ret := keeper.GetRecordFeeAsCoin(ctx)
+
+	paramCoin := sdk.NewInt64Coin(testParams.Denom, int64(testParams.FeeRecord))
+
+	require.True(t, ret.IsEqual(paramCoin))
+}
+
+func TestGetZeroFeeAsCoins(t *testing.T) {
+	ctx, _, keeper := createTestInput(t, false, 100, 0)
+	keeper.SetParams(ctx, testParams)
+
+	ret := keeper.GetZeroFeeAsCoins(ctx)
+
+	paramCoin := sdk.Coins{sdk.NewInt64Coin(testParams.Denom, 0)}
+
+	require.True(t, ret.IsEqual(paramCoin))
+}
+
+func TestGetRegistrationFeeAsCoins(t *testing.T) {
+	ctx, _, keeper := createTestInput(t, false, 100, 0)
+	keeper.SetParams(ctx, testParams)
+
+	ret := keeper.GetRegistrationFeeAsCoins(ctx)
+
+	paramCoin := sdk.Coins{sdk.NewInt64Coin(testParams.Denom, int64(testParams.FeeRegister))}
+
+	require.True(t, ret.IsEqual(paramCoin))
+}
+
+func TestGetRecordFeeAsCoins(t *testing.T) {
+	ctx, _, keeper := createTestInput(t, false, 100, 0)
+	keeper.SetParams(ctx, testParams)
+
+	ret := keeper.GetRecordFeeAsCoins(ctx)
+
+	paramCoin := sdk.Coins{sdk.NewInt64Coin(testParams.Denom, int64(testParams.FeeRecord))}
+
+	require.True(t, ret.IsEqual(paramCoin))
 }
---
+++ x/wrkchain/internal/types/params_test.go
@@ -0,0 +1,48 @@
+package types
+
+import (
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func TestNewParams(t *testing.T) {
+	params := NewParams(RegFee, RecordFee, FeeDenom)
+
+	require.Equal(t, uint64(RegFee), params.FeeRegister)
+	require.Equal(t, uint64(RecordFee), params.FeeRecord)
+	require.Equal(t, FeeDenom, params.Denom)
+}
+
+func TestDefaultParams(t *testing.T) {
+	params := DefaultParams()
+
+	require.Equal(t, uint64(RegFee), params.FeeRegister)
+	require.Equal(t, uint64(RecordFee), params.FeeRecord)
+	require.Equal(t, FeeDenom, params.Denom)
+}
+
+func TestParams_Validate(t *testing.T) {
+	params1 := DefaultParams()
+	err := params1.Validate()
+	require.NoError(t, err)
+
+	params2 := NewParams(RegFee, RecordFee, FeeDenom)
+	err = params2.Validate()
+	require.NoError(t, err)
+
+	params3 := Params{}
+	err = params3.Validate()
+	require.Equal(t, "fee denom cannot be blank", err.Error())
+
+	params3.Denom = "test"
+	err = params3.Validate()
+	require.Equal(t, "registration fee must be positive: 0", err.Error())
+
+	params3.FeeRegister = 1
+	err = params3.Validate()
+	require.Equal(t, "record fee must be positive: 0", err.Error())
+
+	params3.FeeRecord = 1
+	err = params3.Validate()
+	require.NoError(t, err)
+}

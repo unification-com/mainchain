acdf4ff653eb1ea7bbbcca6612ba28f39b4f17b8
Codegnosis <paul@codegnosis.co.uk>
2021-08-18 13:21:56 +0100

    x.enterprise - fix typos

--- x/enterprise/module.go
+++ x/enterprise/module.go
@@ -19,13 +19,12 @@ import (
 	"github.com/cosmos/cosmos-sdk/types/module"
 	simtypes "github.com/cosmos/cosmos-sdk/types/simulation"
 
+	"github.com/unification-com/mainchain/x/enterprise/keeper"
 	"github.com/unification-com/mainchain/x/enterprise/simulation"
 	"github.com/unification-com/mainchain/x/enterprise/types"
-	"github.com/unification-com/mainchain/x/enterprise/keeper"
 
 	"github.com/unification-com/mainchain/x/enterprise/client/cli"
 	"github.com/unification-com/mainchain/x/enterprise/client/rest"
-
 )
 
 var (
@@ -34,28 +33,28 @@ var (
 	_ module.AppModuleSimulation = AppModule{}
 )
 
-// AppModuleBasic defines the basic application module used by the auth module.
+// AppModuleBasic defines the basic application module used by the enterprise module.
 type AppModuleBasic struct{
 	cdc              codec.Marshaler
 }
 
-// Name returns the auth module's name.
+// Name returns the enterprise module's name.
 func (AppModuleBasic) Name() string {
 	return types.ModuleName
 }
 
-// RegisterLegacyAminoCodec registers the auth module's types for the given codec.
+// RegisterLegacyAminoCodec registers the enterprise module's types for the given codec.
 func (AppModuleBasic) RegisterLegacyAminoCodec(cdc *codec.LegacyAmino) {
 	types.RegisterLegacyAminoCodec(cdc)
 }
 
-// DefaultGenesis returns default genesis state as raw bytes for the auth
+// DefaultGenesis returns default genesis state as raw bytes for the enterprise
 // module.
 func (AppModuleBasic) DefaultGenesis(cdc codec.JSONMarshaler) json.RawMessage {
 	return cdc.MustMarshalJSON(types.DefaultGenesisState())
 }
 
-// ValidateGenesis performs genesis state validation for the auth module.
+// ValidateGenesis performs genesis state validation for the enterprise module.
 func (AppModuleBasic) ValidateGenesis(cdc codec.JSONMarshaler, config client.TxEncodingConfig, bz json.RawMessage) error {
 	var data types.GenesisState
 	if err := cdc.UnmarshalJSON(bz, &data); err != nil {
@@ -65,39 +64,39 @@ func (AppModuleBasic) ValidateGenesis(cdc codec.JSONMarshaler, config client.TxE
 	return types.ValidateGenesis(data)
 }
 
-// RegisterRESTRoutes registers the REST routes for the auth module.
+// RegisterRESTRoutes registers the REST routes for the enterprise module.
 func (AppModuleBasic) RegisterRESTRoutes(clientCtx client.Context, rtr *mux.Router) {
 	rest.RegisterLegacyRESTRoutes(clientCtx, rtr)
 }
 
-// RegisterGRPCGatewayRoutes registers the gRPC Gateway routes for the auth module.
+// RegisterGRPCGatewayRoutes registers the gRPC Gateway routes for the enterprise module.
 func (AppModuleBasic) RegisterGRPCGatewayRoutes(clientCtx client.Context, mux *runtime.ServeMux) {
 	types.RegisterQueryHandlerClient(context.Background(), mux, types.NewQueryClient(clientCtx))
 }
 
-// GetTxCmd returns the root tx command for the auth module.
+// GetTxCmd returns the root tx command for the enterprise module.
 func (AppModuleBasic) GetTxCmd() *cobra.Command {
 	return cli.GetTxCmd()
 }
 
-// GetQueryCmd returns the root query command for the auth module.
+// GetQueryCmd returns the root query command for the enterprise module.
 func (AppModuleBasic) GetQueryCmd() *cobra.Command {
 	return cli.GetQueryCmd()
 }
 
-// RegisterInterfaces registers interfaces and implementations of the auth module.
+// RegisterInterfaces registers interfaces and implementations of the enterprise module.
 func (AppModuleBasic) RegisterInterfaces(registry codectypes.InterfaceRegistry) {
 	types.RegisterInterfaces(registry)
 }
 
 //____________________________________________________________________________
 
-// AppModule implements an application module for the auth module.
+// AppModule implements an application module for the enterprise module.
 type AppModule struct {
 	AppModuleBasic
 
-	keeper       keeper.Keeper
-	bankKeeper types.BankKeeper
+	keeper        keeper.Keeper
+	bankKeeper    types.BankKeeper
 	accountKeeper types.AccountKeeper
 }
 
@@ -111,7 +110,7 @@ func NewAppModule(cdc codec.Marshaler, keeper keeper.Keeper, bankKeeper types.Ba
 	}
 }
 
-// Name returns the auth module's name.
+// Name returns the enterprise module's name.
 func (AppModule) Name() string {
 	return types.ModuleName
 }
@@ -126,12 +125,12 @@ func (am AppModule) Route() sdk.Route {
 	return sdk.NewRoute(types.RouterKey, NewHandler(am.keeper))
 }
 
-// QuerierRoute returns the auth module's querier route name.
+// QuerierRoute returns the enterprise module's querier route name.
 func (AppModule) QuerierRoute() string {
 	return types.QuerierRoute
 }
 
-// LegacyQuerierHandler returns the auth module sdk.Querier.
+// LegacyQuerierHandler returns the enterprise module sdk.Querier.
 func (am AppModule) LegacyQuerierHandler(legacyQuerierCdc *codec.LegacyAmino) sdk.Querier {
 	return keeper.NewLegacyQuerier(am.keeper, legacyQuerierCdc)
 }
@@ -143,7 +142,7 @@ func (am AppModule) RegisterServices(cfg module.Configurator) {
 	types.RegisterQueryServer(cfg.QueryServer(), am.keeper)
 }
 
-// InitGenesis performs genesis initialization for the auth module. It returns
+// InitGenesis performs genesis initialization for the enterprise module. It returns
 // no validator updates.
 func (am AppModule) InitGenesis(ctx sdk.Context, cdc codec.JSONMarshaler, data json.RawMessage) []abci.ValidatorUpdate {
 	var genesisState types.GenesisState
@@ -152,19 +151,19 @@ func (am AppModule) InitGenesis(ctx sdk.Context, cdc codec.JSONMarshaler, data j
 	return []abci.ValidatorUpdate{}
 }
 
-// ExportGenesis returns the exported genesis state as raw bytes for the auth
+// ExportGenesis returns the exported genesis state as raw bytes for the enterprise
 // module.
 func (am AppModule) ExportGenesis(ctx sdk.Context, cdc codec.JSONMarshaler) json.RawMessage {
 	gs := ExportGenesis(ctx, am.keeper)
 	return cdc.MustMarshalJSON(gs)
 }
 
-// BeginBlock returns the begin blocker for the auth module.
+// BeginBlock returns the begin blocker for the enterprise module.
 func (am AppModule) BeginBlock(ctx sdk.Context, _ abci.RequestBeginBlock) {
 	BeginBlocker(ctx, am.keeper)
 }
 
-// EndBlock returns the end blocker for the auth module. It returns no validator
+// EndBlock returns the end blocker for the enterprise module. It returns no validator
 // updates.
 func (AppModule) EndBlock(_ sdk.Context, _ abci.RequestEndBlock) []abci.ValidatorUpdate {
 	return []abci.ValidatorUpdate{}
@@ -174,7 +173,7 @@ func (AppModule) EndBlock(_ sdk.Context, _ abci.RequestEndBlock) []abci.Validato
 
 // AppModuleSimulation functions
 
-// GenerateGenesisState creates a randomized GenState of the auth module
+// GenerateGenesisState creates a randomized GenState of the enterprise module
 func (am AppModule) GenerateGenesisState(simState *module.SimulationState) {
 	simulation.RandomizedGenState(simState)
 }
@@ -184,17 +183,17 @@ func (AppModule) ProposalContents(simState module.SimulationState) []simtypes.We
 	return nil
 }
 
-// RandomizedParams creates randomized auth param changes for the simulator.
+// RandomizedParams creates randomized enterprise param changes for the simulator.
 func (AppModule) RandomizedParams(r *rand.Rand) []simtypes.ParamChange {
 	return simulation.ParamChanges(r)
 }
 
-// RegisterStoreDecoder registers a decoder for auth module's types
+// RegisterStoreDecoder registers a decoder for enterprise module's types
 func (am AppModule) RegisterStoreDecoder(sdr sdk.StoreDecoderRegistry) {
 	sdr[types.StoreKey] = simulation.NewDecodeStore(am.cdc)
 }
 
-// WeightedOperations doesn't return any auth module operation.
+// WeightedOperations doesn't return any enterprise module operation.
 func (am AppModule) WeightedOperations(simState module.SimulationState) []simtypes.WeightedOperation {
 	return simulation.WeightedOperations(
 		simState.AppParams, simState.Cdc,

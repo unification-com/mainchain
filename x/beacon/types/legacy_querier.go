24d1ac1c5e6816200cf0c1e957b74c84559c9e59
Codegnosis <paul@codegnosis.co.uk>
2022-01-11 09:59:42 +0000

    x.beacon - update unit tests

--- x/beacon/types/legacy_querier.go
+++ x/beacon/types/legacy_querier.go
@@ -1,11 +1,10 @@
 package types
 
-import (
-	sdk "github.com/cosmos/cosmos-sdk/types"
-)
-
 // QueryResBeaconTimestampHashes Queries Result Payload for a Beacon timestamp Hashes query
 type QueryResBeaconTimestampHashes []BeaconTimestamp
+
+// QueryResBeacons Queries BEACONs
+type QueryResBeacons []Beacon
 
 //// implement fmt.Stringer
 //func (h QueryResBeaconTimestampHashes) String() (out string) {
@@ -26,40 +25,40 @@ type QueryResBeaconTimestampHashes []BeaconTimestamp
 //	return strings.TrimSpace(out)
 //}
 
-// QueryBeaconParams Params for query 'custom/beacon/registered'
-type QueryBeaconParams struct {
-	Page    int
-	Limit   int
-	Moniker string
-	Owner   sdk.AccAddress
-}
-
-// NewQueryBeaconParams creates a new instance of QueryBeaconParams
-func NewQueryBeaconParams(page, limit int, monkker string, owner sdk.AccAddress) QueryBeaconParams {
-	return QueryBeaconParams{
-		Page:    page,
-		Limit:   limit,
-		Moniker: monkker,
-		Owner:   owner,
-	}
-}
+//// QueryBeaconParams Params for query 'custom/beacon/registered'
+//type QueryBeaconParams struct {
+//	Page    int
+//	Limit   int
+//	Moniker string
+//	Owner   sdk.AccAddress
+//}
+//
+//// NewQueryBeaconParams creates a new instance of QueryBeaconParams
+//func NewQueryBeaconParams(page, limit int, monkker string, owner sdk.AccAddress) QueryBeaconParams {
+//	return QueryBeaconParams{
+//		Page:    page,
+//		Limit:   limit,
+//		Moniker: monkker,
+//		Owner:   owner,
+//	}
+//}
 
 // QueryBeaconTimestampParams Params for query 'custom/beacon/timestamps'
-type QueryBeaconTimestampParams struct {
-	BeaconID   uint64
-	Page       int
-	Limit      int
-	SubmitTime uint64
-	Hash       string
-}
-
-// NewQueryBeaconTimestampParams creates a new instance of QueryBeaconTimestampParams
-func NewQueryBeaconTimestampParams(page, limit int, beaconID uint64, hash string, submitTime uint64) QueryBeaconTimestampParams {
-	return QueryBeaconTimestampParams{
-		BeaconID:   beaconID,
-		Page:       page,
-		Limit:      limit,
-		SubmitTime: submitTime,
-		Hash:       hash,
-	}
-}
+//type QueryBeaconTimestampParams struct {
+//	BeaconID   uint64
+//	Page       int
+//	Limit      int
+//	SubmitTime uint64
+//	Hash       string
+//}
+//
+//// NewQueryBeaconTimestampParams creates a new instance of QueryBeaconTimestampParams
+//func NewQueryBeaconTimestampParams(page, limit int, beaconID uint64, hash string, submitTime uint64) QueryBeaconTimestampParams {
+//	return QueryBeaconTimestampParams{
+//		BeaconID:   beaconID,
+//		Page:       page,
+//		Limit:      limit,
+//		SubmitTime: submitTime,
+//		Hash:       hash,
+//	}
+//}

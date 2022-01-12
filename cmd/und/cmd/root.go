b4739fd3a989b1c3daeab4960ba1258824895d84
Codegnosis <paul@codegnosis.co.uk>
2022-01-12 11:25:28 +0000

    cmd - plug in convert command

--- cmd/und/cmd/root.go
+++ cmd/und/cmd/root.go
@@ -98,6 +98,7 @@ func initRootCmd(rootCmd *cobra.Command, encodingConfig params.EncodingConfig) {
 		queryCommand(),
 		txCommand(),
 		keys.Commands(app.DefaultNodeHome),
+		GetDenomConversionCmd(),
 	)
 }
 

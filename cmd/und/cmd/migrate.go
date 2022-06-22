7fab53d7ccdf1ac735f365886d0138dce99ea7d7
Codegnosis <paul@codegnosis.co.uk>
2022-06-22 11:13:06 +0100

    cmd - updates for Cosmos SDK v0.45.x

--- cmd/und/cmd/migrate.go
+++ cmd/und/cmd/migrate.go
@@ -1,23 +1,11 @@
 package cmd
 
 import (
-	"encoding/json"
 	"fmt"
-	"github.com/cosmos/cosmos-sdk/types/errors"
-	tmtypes "github.com/tendermint/tendermint/types"
-	"time"
-
-	"github.com/cosmos/cosmos-sdk/client"
 	"github.com/cosmos/cosmos-sdk/client/flags"
-	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/version"
-	v039 "github.com/cosmos/cosmos-sdk/x/genutil/legacy/v039"
-	v040 "github.com/cosmos/cosmos-sdk/x/genutil/legacy/v040"
-	"github.com/cosmos/cosmos-sdk/x/genutil/types"
 	"github.com/spf13/cobra"
-	tmjson "github.com/tendermint/tendermint/libs/json"
-
-	v040fund "github.com/unification-com/mainchain/migrate/v040"
+	tmtypes "github.com/tendermint/tendermint/types"
 )
 
 const flagGenesisTime = "genesis-time"
@@ -33,64 +21,9 @@ func MigrateGenesisCmd() *cobra.Command {
 Example:
 $ %s migrate /path/to/genesis.json --chain-id=FUND-Main-2 --genesis-time=2022-06-01T10:00:00Z
 `, version.AppName),
-		Args: cobra.ExactArgs(1),
+		Args: cobra.ExactArgs(0),
 		RunE: func(cmd *cobra.Command, args []string) error {
-			clientCtx := client.GetClientContextFromCmd(cmd)
-
-			var err error
-
-			importGenesis := args[0]
-
-			genDoc, err := validateGenDoc(importGenesis)
-			if err != nil {
-				return err
-			}
-
-			var initialState types.AppMap
-			if err := json.Unmarshal(genDoc.AppState, &initialState); err != nil {
-				return errors.Wrap(err, "failed to JSON unmarshal initial genesis state")
-			}
-
-			// Run 0.38 -> 0.40 for Cosmos modules
-			newGenState := v039.Migrate(initialState, clientCtx)
-			newGenState = v040.Migrate(newGenState, clientCtx)
-
-			// Run und modules 0.38 -> 0.40
-			newGenState = v040fund.Migrate(newGenState, clientCtx)
-
-			genDoc.AppState, err = json.Marshal(newGenState)
-			if err != nil {
-				return errors.Wrap(err, "failed to JSON marshal migrated genesis state")
-			}
-
-			genesisTime, _ := cmd.Flags().GetString(flagGenesisTime)
-			if genesisTime != "" {
-				var t time.Time
-
-				err := t.UnmarshalText([]byte(genesisTime))
-				if err != nil {
-					return errors.Wrap(err, "failed to unmarshal genesis time")
-				}
-
-				genDoc.GenesisTime = t
-			}
-
-			chainID, _ := cmd.Flags().GetString(flags.FlagChainID)
-			if chainID != "" {
-				genDoc.ChainID = chainID
-			}
-
-			bz, err := tmjson.Marshal(genDoc)
-			if err != nil {
-				return errors.Wrap(err, "failed to marshal genesis doc")
-			}
-
-			sortedBz, err := sdk.SortJSON(bz)
-			if err != nil {
-				return errors.Wrap(err, "failed to sort JSON genesis doc")
-			}
-
-			fmt.Println(string(sortedBz))
+			// no migration script
 			return nil
 		},
 	}

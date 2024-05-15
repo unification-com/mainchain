7d1302ffedc3426597227823fc4141d47c8aabf4
Codegnosis <paul@codegnosis.co.uk>
2024-05-15 09:31:23 +0100

    x.enterprise - initial changes for cosmos sdk v0.47.x

--- x/enterprise/module.go
+++ x/enterprise/module.go
@@ -4,8 +4,6 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
-	"math/rand"
-
 	"github.com/grpc-ecosystem/grpc-gateway/runtime"
 
 	abci "github.com/cometbft/cometbft/abci/types"
@@ -119,20 +117,20 @@ func (am AppModule) RegisterInvariants(ir sdk.InvariantRegistry) {
 	keeper.RegisterInvariants(ir, am.keeper)
 }
 
-// Route returns the message routing key for the module.
-func (am AppModule) Route() sdk.Route {
-	return sdk.NewRoute(types.RouterKey, NewHandler(am.keeper))
-}
+//// Route returns the message routing key for the module.
+//func (am AppModule) Route() sdk.Route {
+//	return sdk.NewRoute(types.RouterKey, NewHandler(am.keeper))
+//}
 
 // QuerierRoute returns the enterprise module's querier route name.
 func (AppModule) QuerierRoute() string {
 	return types.QuerierRoute
 }
 
-// LegacyQuerierHandler returns the enterprise module sdk.Querier.
-func (am AppModule) LegacyQuerierHandler(legacyQuerierCdc *codec.LegacyAmino) sdk.Querier {
-	return keeper.NewLegacyQuerier(am.keeper, legacyQuerierCdc)
-}
+//// LegacyQuerierHandler returns the enterprise module sdk.Querier.
+//func (am AppModule) LegacyQuerierHandler(legacyQuerierCdc *codec.LegacyAmino) sdk.Querier {
+//	return keeper.NewLegacyQuerier(am.keeper, legacyQuerierCdc)
+//}
 
 // RegisterServices registers a GRPC query service to respond to the
 // module-specific GRPC queries.
@@ -188,10 +186,10 @@ func (AppModule) ProposalContents(simState module.SimulationState) []simtypes.We
 	return nil
 }
 
-// RandomizedParams creates randomized enterprise param changes for the simulator.
-func (AppModule) RandomizedParams(r *rand.Rand) []simtypes.ParamChange {
-	return simulation.ParamChanges(r)
-}
+//// RandomizedParams creates randomized enterprise param changes for the simulator.
+//func (AppModule) RandomizedParams(r *rand.Rand) []simtypes.ParamChange {
+//	return simulation.ParamChanges(r)
+//}
 
 // RegisterStoreDecoder registers a decoder for enterprise module's types
 func (am AppModule) RegisterStoreDecoder(sdr sdk.StoreDecoderRegistry) {

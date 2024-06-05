4ac772ce1650986b573d9c557d7799c17b943b1d
Codegnosis <paul@codegnosis.co.uk>
2024-06-05 14:05:36 +0100

    x.stream - implement tx commands

--- x/stream/client/cli/tx.go
+++ x/stream/client/cli/tx.go
@@ -39,6 +39,10 @@ func GetTxCmd() *cobra.Command {
 	cmd.AddCommand(
 		GetCmdCreateStream(),
 		GetCmdClaimStream(),
+		GetCmdClaimStreamById(),
+		GetCmdTopUpDeposit(),
+		GetCmdUpdateFlowRate(),
+		GetCmdCancelStream(),
 	)
 
 	return cmd
@@ -94,15 +98,54 @@ $ %s tx %s create und173qnkw458p646fahmd53xa45vqqvga7kyu6ryy 777000000000nund 29
 	return cmd
 }
 
-// GetCmdClaimStream is the CLI command for claiming funds held in a stream
+// GetCmdClaimStreamById is the CLI command for claiming funds held in a stream
 func GetCmdClaimStream() *cobra.Command {
 	cmd := &cobra.Command{
-		Use:   "claim [stream_id]",
-		Short: "Claim funds held in a stream",
+		Use:   "claim-by-sender [sender_Addr]",
+		Short: "Claim funds held in a stream by stream sender address",
 		Long: strings.TrimSpace(
-			fmt.Sprintf(`Claim funds held in a stream
+			fmt.Sprintf(`Claim funds held in a stream by sender address
 Example:
-$ %s tx %s claim 1 --from t1
+$ %s tx %s claim-by-sender und173qnkw458p646fahmd53xa45vqqvga7kyu6ryy --from t1
+`,
+				version.AppName, types.ModuleName,
+			),
+		),
+		Args: cobra.ExactArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) error {
+
+			clientCtx, err := client.GetClientTxContext(cmd)
+			if err != nil {
+				return err
+			}
+			receiver := clientCtx.GetFromAddress()
+
+			sender, err := sdk.AccAddressFromBech32(args[0])
+			if err != nil {
+				return err
+			}
+
+			msg := types.NewMsgClaimStream(sender, receiver)
+			if err := msg.ValidateBasic(); err != nil {
+				return err
+			}
+
+			return tx.GenerateOrBroadcastTxCLI(clientCtx, cmd.Flags(), msg)
+		},
+	}
+	flags.AddTxFlagsToCmd(cmd)
+	return cmd
+}
+
+// GetCmdClaimStreamById is the CLI command for claiming funds held in a stream
+func GetCmdClaimStreamById() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "claim-by-id [stream_id]",
+		Short: "Claim funds held in a stream by stream id",
+		Long: strings.TrimSpace(
+			fmt.Sprintf(`Claim funds held in a stream by stream id
+Example:
+$ %s tx %s claim-by-id 1 --from t1
 `,
 				version.AppName, types.ModuleName,
 			),
@@ -121,7 +164,134 @@ $ %s tx %s claim 1 --from t1
 				return err
 			}
 
-			msg := types.NewMsgClaimStream(streamId, receiver)
+			msg := types.NewMsgClaimStreamById(streamId, receiver)
+			if err := msg.ValidateBasic(); err != nil {
+				return err
+			}
+
+			return tx.GenerateOrBroadcastTxCLI(clientCtx, cmd.Flags(), msg)
+		},
+	}
+	flags.AddTxFlagsToCmd(cmd)
+	return cmd
+}
+
+// GetCmdTopUpDeposit is the CLI command for topping up deposit in a stream
+func GetCmdTopUpDeposit() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "topup [stream_id] [deposit]",
+		Short: "Top up deposit in a stream",
+		Long: strings.TrimSpace(
+			fmt.Sprintf(`Top up deposit in a stream
+Example:
+$ %s tx %s topup 1 100000000000nund --from t1
+`,
+				version.AppName, types.ModuleName,
+			),
+		),
+		Args: cobra.ExactArgs(2),
+		RunE: func(cmd *cobra.Command, args []string) error {
+
+			clientCtx, err := client.GetClientTxContext(cmd)
+			if err != nil {
+				return err
+			}
+			sender := clientCtx.GetFromAddress()
+
+			streamId, err := strconv.ParseUint(args[0], 10, 64)
+			if err != nil {
+				return err
+			}
+
+			deposit, err := sdk.ParseCoinNormalized(args[1])
+			if err != nil {
+				return err
+			}
+
+			msg := types.NewMsgTopUpDeposit(streamId, deposit, sender)
+			if err := msg.ValidateBasic(); err != nil {
+				return err
+			}
+
+			return tx.GenerateOrBroadcastTxCLI(clientCtx, cmd.Flags(), msg)
+		},
+	}
+	flags.AddTxFlagsToCmd(cmd)
+	return cmd
+}
+
+// GetCmdUpdateFlowRate is the CLI command for updating the flow rate of a stream
+func GetCmdUpdateFlowRate() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "update-flow [stream_id] [new_flow_rate]",
+		Short: "Change the flow rate of a stream",
+		Long: strings.TrimSpace(
+			fmt.Sprintf(`Change the flow rate of a stream
+Example:
+$ %s tx %s update-flow 1 246973 --from t1
+`,
+				version.AppName, types.ModuleName,
+			),
+		),
+		Args: cobra.ExactArgs(2),
+		RunE: func(cmd *cobra.Command, args []string) error {
+
+			clientCtx, err := client.GetClientTxContext(cmd)
+			if err != nil {
+				return err
+			}
+			sender := clientCtx.GetFromAddress()
+
+			streamId, err := strconv.ParseUint(args[0], 10, 64)
+			if err != nil {
+				return err
+			}
+
+			flowRate, err := strconv.ParseInt(args[1], 10, 64)
+			if err != nil {
+				return err
+			}
+
+			msg := types.NewMsgUpdateFlowRate(streamId, flowRate, sender)
+			if err := msg.ValidateBasic(); err != nil {
+				return err
+			}
+
+			return tx.GenerateOrBroadcastTxCLI(clientCtx, cmd.Flags(), msg)
+		},
+	}
+	flags.AddTxFlagsToCmd(cmd)
+	return cmd
+}
+
+// GetCmdCancelStream is the CLI command for cancelling a stream
+func GetCmdCancelStream() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "cancel [stream_id]",
+		Short: "Cancel a stream",
+		Long: strings.TrimSpace(
+			fmt.Sprintf(`Cancel a stream
+Example:
+$ %s tx %s cancel 1 --from t1
+`,
+				version.AppName, types.ModuleName,
+			),
+		),
+		Args: cobra.ExactArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) error {
+
+			clientCtx, err := client.GetClientTxContext(cmd)
+			if err != nil {
+				return err
+			}
+			sender := clientCtx.GetFromAddress()
+
+			streamId, err := strconv.ParseUint(args[0], 10, 64)
+			if err != nil {
+				return err
+			}
+
+			msg := types.NewMsgCancelStream(streamId, sender)
 			if err := msg.ValidateBasic(); err != nil {
 				return err
 			}

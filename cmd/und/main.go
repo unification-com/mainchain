18d172976eab30eb06570611c925560f06577225
Codegnosis <paul@codegnosis.co.uk>
2025-07-23 11:27:11 +0100

    app, cmd - seal sdk config

--- cmd/und/main.go
+++ cmd/und/main.go
@@ -2,11 +2,12 @@ package main
 
 import (
 	"fmt"
-	"github.com/unification-com/mainchain/app"
 	"os"
 
 	svrcmd "github.com/cosmos/cosmos-sdk/server/cmd"
+	sdk "github.com/cosmos/cosmos-sdk/types"
 
+	"github.com/unification-com/mainchain/app"
 	appparams "github.com/unification-com/mainchain/app/params"
 	"github.com/unification-com/mainchain/cmd/und/cmd"
 )
@@ -14,6 +15,8 @@ import (
 func main() {
 	// Set config for address prefixes to "und", hd path to 5555 etc.
 	appparams.SetAddressPrefixes()
+	config := sdk.GetConfig()
+	config.Seal()
 
 	rootCmd := cmd.NewRootCmd()
 	if err := svrcmd.Execute(rootCmd, "", app.DefaultNodeHome); err != nil {

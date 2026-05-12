ae65166931eb7a3cc67f2b1f387759a8e2886f8b
Codegnosis <paul@codegnosis.co.uk>
2026-05-12 14:45:20 +0100

    proto - refresh buf.lock and regenerate pb.go against SDK v0.54

--- x/wrkchain/types/query.pb.go
+++ x/wrkchain/types/query.pb.go
@@ -829,6 +829,7 @@ func _Query_WrkChainStorage_Handler(srv interface{}, ctx context.Context, dec fu
 	return interceptor(ctx, in, info, handler)
 }
 
+var Query_serviceDesc = _Query_serviceDesc
 var _Query_serviceDesc = grpc.ServiceDesc{
 	ServiceName: "mainchain.wrkchain.v1.Query",
 	HandlerType: (*QueryServer)(nil),
